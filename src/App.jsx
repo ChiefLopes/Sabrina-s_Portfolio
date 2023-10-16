c40d07677d8e764e799d46a1ed177857516c304a
ChiefLopes <chieflopesekede@gmail.com>
2023-10-16 15:43:59 +0100

    initial commit of react files and components needed for the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-white-400">
+        <span className="text-7xl text-green-500">GOD</span> over all
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>Treasue</div>
+  )
+}
+
+export default NavBar
