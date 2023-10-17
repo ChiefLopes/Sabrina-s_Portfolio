5013c22802d8a7dd021e9b2142bf980d6625ddcb
ChiefLopes <chieflopesekede@gmail.com>
2023-10-17 14:48:43 +0100

    refactored the Navbar page by mapping through the Options

--- src/Components/Home/NavBar.jsx
+++ src/Components/Home/NavBar.jsx
@@ -1,17 +1,25 @@
 const NavBar = () => {
+  const menuItems = [
+    "Home",
+    "About",
+    "Services",
+    "How it works",
+    "Reviews",
+    "Portfolio",
+  ];
+
   return (
-    <div className="flex justify-between pt-8 ">
+    <div className="flex justify-between pt-8">
       <div>
-        <h1 className="text-3xl text-blue-400 px-[100px]">LAJ</h1>
+        <h1 className="text-3xl text-blue-400 px-10">LAJ</h1>
       </div>
       <nav>
-        <ul className="flex justify-between space-x-7  px-[160px]">
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>How it works</li>
-          <li>Reviews</li>
-          <li>Portfolio</li>
+        <ul className="flex justify-between space-x-7 px-10">
+          {menuItems.map((item, index) => (
+            <li key={index} className="cursor-pointer">
+              {item}
+            </li>
+          ))}
           <li>
             <button className="bg-blue-400 flex justify-center items-center text-white rounded-md h-10 w-20 text-sm font-bold">
               <span className="whitespace-nowrap">Hire me</span>
@@ -22,5 +30,6 @@ const NavBar = () => {
     </div>
   );
 };
+
 
 export default NavBar;
